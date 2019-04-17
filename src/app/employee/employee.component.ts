8b57dff4a07b16faf3a29f55a560a0eea053e72b
vigneshbv02 <vigneshbv02@gmail.com>
2019-04-17 11:06:20 +0530

    2 files modified

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -14,7 +14,11 @@ export class EmployeeComponent implements OnInit {
 
   ngOnInit() {
     this.addEmployee = this.formBuilder.group({
-      
+      username : new FormControl("", [Validators.required]),
+      phone: new FormControl("", [Validators.required]),
+      email: new FormControl("", [Validators.required, Validators.pattern('[a-z0-9]+@[a-z].[a-z]{3}$')]),
+      password: new FormControl("", [Validators.required]),
+      repassword: new FormControl("", [Validators.required])
     });
   }
 
